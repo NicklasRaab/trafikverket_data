6f2610bf63e9e80009a159ab79626614e3f5f149
Nicklas Raab <nicklas.raab@gmail.com>
2020-02-11 23:20:33 +0100

    output data to DOM

--- js/script.js
+++ js/script.js
@@ -17,15 +17,21 @@ const getDataFromTrafikverket = async (api_key) => {
     return dataObject;
 }
 
-function testTest(stuff) {
-  console.log(stuff);
-  return stuff
+function dataOutputWriter(data) {
+  console.log(data);
+  data.noise.RESPONSE.RESULT[0].TrafficFlow.forEach(row => {
+      let element = document.querySelector('#content');
+      let row_data = '<p>' + row.SiteId + ' ' + row.AverageVehicleSpeed + '</p>';
+      element.innerHTML = element.innerHTML + row_data;
+  });
+  // console.log(stuff.noise.RESPONSE.RESULT[0]);
+  return data;
 }
 
 
 const api_key = 'f8dc88a7e302436e8753039d67579de0';
 const coordinates = '356363.36 6213391.38';
 getDataFromTrafikverket(api_key)
-    .then(data => testTest(data))
-    .then(data => console.log(data))
+    .then(data => dataOutputWriter(data))
+    // .then(data => console.log(data))
     .catch(reason => console.log(reason.message));
